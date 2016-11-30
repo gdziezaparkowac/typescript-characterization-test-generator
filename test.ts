e7c51afa096b9a27e5ca293747c24e84ca5fc88d
jerrycoke <azuretestingmy2@gmail.com>
2016-11-30 19:19:48 +0100

    Use example from https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API and add members and exports

---
+++ test.ts
@@ -0,0 +1,24 @@
+/**
+ * Documentation for C
+ */
+
+interface AA {
+  test: () => string
+}
+
+class RR {
+  public static janek(c): number {
+
+  }
+
+  private ale = 'aa';
+
+  private static pup: number = 5;
+
+  test(a: (as: number) => string): boolean {
+
+  }
+  test22(a): string {
+
+  }
+}
